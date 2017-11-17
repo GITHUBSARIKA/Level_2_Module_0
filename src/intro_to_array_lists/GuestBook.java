1752a423df26da7a918493c7080e0fdb7511b5ea
NikitaD111 <league@WTS20.attlocal.net>
2017-11-16 18:27:03 -0800

    Level 2 Module

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,5 +1,9 @@
 package intro_to_array_lists;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -9,4 +13,19 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	public static void main(String[] args) {
+
+	
+	}
+	public GuestBook() {
+			JFrame frame=new JFrame();
+		JPanel panel=new JPanel();
+		JButton button1=new JButton();
+		JButton button2=new JButton();
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		button1.setText("Add Name");
+		button2.setText("View Names");
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,9 +1,35 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
+		ArrayList<String> animals=new ArrayList<String>();
+		animals.add("dog");
+		animals.add("cat");
+		animals.add("horse");
+		animals.add("dolphin");
+		animals.add("tiger");
+		//for (int i = 0; i < animals.size(); i++) {
+			//String a=animals.get( i);
+			//System.out.println("String at element " + i + " " + a);
+		//}
+		//for(String a:animals) {
+			//System.out.println(a);
+		//}
+		//for (int a = 0; a < animals.size(); a+=2) {
+			//System.out.println(animals.get(a));
+		//}
+		//for (int a = animals.size()-1; a >=0; a--) {
+			//System.out.println(animals.get(a));
+		//}
+	       for(String a:animals) {
+	    	   if (a.contains("e")) {
+				System.out.println(a);
+			}
+		}
 		
 		//2. Add five Strings to your list
 		
